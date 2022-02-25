d05d67a5dce8db445962aab7b95ff1f62adb1fad
Zedfeld Businos <nxt.fe.dev.two@gmail.com>
2022-02-25 13:13:27 +0800

    add prettier and eslint

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const accountRoute = require('./routes/accountRoute')
+const todosRoute = require('./routes/todosRoute')
 
 app.use(express.json())
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`))
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.use('/api/account', accountRoute)
+app.use('/api/todos', todosRoute)
